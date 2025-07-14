6c2fc3d6e28ddd0a6f17ddc2814ad7a2c77fd045
Shivanshu <shivanshu8398@gmail.com>
2025-07-14 11:55:50 +0530

    Update vite.config.js to use port 3000

--- Question1/Frontend/vite.config.js
+++ Question1/Frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // 👈 This forces Vite to run on http://localhost:3000
+  },
+});
